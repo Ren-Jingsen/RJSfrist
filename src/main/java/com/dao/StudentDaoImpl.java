67d30f49b54e69e0ea85126b0762fd736344f672
Renjingsen <2876188845@qq.com>
2019-11-21 12:31:42 +0800

    第二次提交项目

--- src/main/java/com/dao/StudentDaoImpl.java
+++ src/main/java/com/dao/StudentDaoImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class StudentDaoImpl implements  StudentDao {
 
         public List<Emp> selectAll(){
-
+            System.out.println("q23");
 
             return null;
         }
